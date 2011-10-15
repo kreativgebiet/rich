30300236c8f13ab89bfec9ceb981a7e101d298fa
Bastiaan Terhorst <bastiaan@perceptor.nl>
2011-10-15 15:59:03 +0200

    create install generator

--- test/dummy/config/initializers/rich.rb
+++ test/dummy/config/initializers/rich.rb
@@ -4,13 +4,10 @@ if Object.const_defined?("Rich")
   Rich.setup do |config|    
     
     # Customize ckeditor by adding configuration to config.editor (you can use any ckeditor config directive)
-    #config.editor[:skin] = 'office2003' # ill advice, but you get the idea
-    
-    #STUB
-    # Customize the models Rich will use for file uploads
-    # if you used the generator, you shouldn't need to change these
-    #config.image_model = 'Rich::ImageAsset'
-    
+    # Look at the Rich source for default settings. These settings can also be overridden per editor instance.
+    # Example: config.editor[:skin] = 'office2003' # ill advice, but you get the idea
+    # Example: config.editor[:startupOutlineBlocks] = false
+
     config.image_styles = {
       :large => "500x500",
       :thumb => "100x100#"
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
+  mount Rich::Engine => '/rich'
+
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
     resources :posts
-
-    mount Rich::Engine => "/rich"
     
     resources :test
     root :to => 'posts#index'
