81f0bb5fbfb0a2bf95b44675696e119c5a65ab70
Bastiaan Terhorst <bastiaan@perceptor.nl>
2011-11-18 23:26:34 +0100

    improve support for both formtastic 1 and 2

--- app/inputs/rich_asset_input.rb
+++ app/inputs/rich_asset_input.rb
@@ -1,13 +1,20 @@
-class RichAssetInput < ::Formtastic::Inputs::StringInput  
+
+if Object.const_defined?("Formtastic")
+  if(Gem.loaded_specs["formtastic"].version.version[0,1] == "2")
+
+    class RichAssetInput < ::Formtastic::Inputs::StringInput  
   
-  # todo:
-  # differentiate between rich_asset and rich_asset_url
-  # where the former links an object and the latter returns a url string
-  # ?
+      # todo:
+      # differentiate between rich_asset and rich_asset_url
+      # where the former links an object and the latter returns a url string
+      # ?
   
-  def input_html_options
-    options = super
-    options[:class] = [options[:class], "rich_asset"].compact.join(' ')
-    options
+      def input_html_options
+        options = super
+        options[:class] = [options[:class], "rich_asset"].compact.join(' ')
+        options
+      end
+    end
+    
   end
 end
--- app/inputs/rich_input.rb
+++ app/inputs/rich_input.rb
@@ -1,15 +1,25 @@
-class RichInput < ::Formtastic::Inputs::TextInput  
-  def to_html
-    editor_options = Rich.editor.merge(options[:editor] || {})
-    editor_options[:width]  = options[:width] || '76%'
-    editor_options[:height] = options[:height] || '400px'
-    
-    input_wrapping do
-      label_html <<
-      builder.text_area(method, input_html_options) <<
-      #javascript_tag("$(function(){$('##{dom_id}').ckeditor(function() { }, #{editor_options.to_json} );});")
-      # doing this might be insecure?
-      "<script>$(function(){$('##{dom_id}').ckeditor(function() { }, #{editor_options.to_json} );});</script>".html_safe
+
+if Object.const_defined?("Formtastic")
+  if(Gem.loaded_specs["formtastic"].version.version[0,1] == "2")
+
+
+    class RichInput < ::Formtastic::Inputs::TextInput  
+      def to_html
+        editor_options = Rich.editor.merge(options[:editor] || {})
+        editor_options[:width]  = options[:width] || '76%'
+        editor_options[:height] = options[:height] || '400px'
+
+        input_wrapping do
+          label_html <<
+          builder.text_area(method, input_html_options) <<
+          #javascript_tag("$(function(){$('##{dom_id}').ckeditor(function() { }, #{editor_options.to_json} );});")
+          # doing this might be insecure?
+          "<script>$(function(){$('##{dom_id}').ckeditor(function() { }, #{editor_options.to_json} );});</script>".html_safe
+        end
+      end
     end
+    
+
   end
 end
+
