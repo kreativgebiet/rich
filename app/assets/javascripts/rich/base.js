612729c95fe2bf923d251c8b1238fe7a7b4f9c53
dunyakirkali <dunyakirkali@gmail.com>
2012-06-27 22:15:50 +0200

    using ckeditor_source.js instead of ckeditor.js

--- app/assets/javascripts/rich/base.js
+++ app/assets/javascripts/rich/base.js
@@ -1,6 +1,6 @@
 // Wire up Rich
 //= require rich/editor/ckeditor_path
-//= require ckeditor/ckeditor
+//= require ckeditor/ckeditor_source
 //= require ckeditor/adapters/jquery
 //= require rich/editor/rich_editor
 //= require rich/editor/rich_picker
