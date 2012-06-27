3b2169ca1929779be9644b86a6358c0358a9e8bd
dunyakirkali <dunyakirkali@gmail.com>
2012-06-27 16:28:01 +0200

    Reverting changes and adding a new test

--- app/assets/javascripts/rich/base.js
+++ app/assets/javascripts/rich/base.js
@@ -1,2 +1,6 @@
 // Wire up Rich
 //= require rich/editor/ckeditor_path
+//= require ckeditor/ckeditor
+//= require ckeditor/adapters/jquery
+//= require rich/editor/rich_editor
+//= require rich/editor/rich_picker
