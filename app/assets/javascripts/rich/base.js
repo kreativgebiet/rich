9abea30dd3943a15d2e49a7436529ffefdc0c7bd
dunyakirkali <dunyakirkali@gmail.com>
2012-06-27 16:09:07 +0200

    Test fixes

--- app/assets/javascripts/rich/base.js
+++ app/assets/javascripts/rich/base.js
@@ -1,6 +1,2 @@
 // Wire up Rich
 //= require rich/editor/ckeditor_path
-//= require ckeditor/ckeditor
-//= require ckeditor/adapters/jquery
-//= require rich/editor/rich_editor
-//= require rich/editor/rich_picker
