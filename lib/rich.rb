4677670639c226defa3a4fd347e10e2cc4118f0c
Bastiaan Terhorst <bastiaan@perceptor.nl>
2012-09-30 15:04:23 +0200

    fix error in file storage type and make it the default for backwards compatibility

--- lib/rich.rb
+++ lib/rich.rb
@@ -46,7 +46,7 @@ module Rich
   @@allowed_document_types = :all
   
   mattr_accessor :file_storage
-  @@file_storage
+  @@file_storage = :filesystem
   
   mattr_accessor :s3_credentials
   @@s3_credentials
