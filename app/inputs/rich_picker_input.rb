e18d902c772be8d873e2e1069a6b40a060d65aca
Daniel Westendorf <daniel@prowestech.com>
2012-10-03 11:26:34 -0600

    fix placeholder_image bug of it now being shown correctly

--- app/inputs/rich_picker_input.rb
+++ app/inputs/rich_picker_input.rb
@@ -13,21 +13,22 @@ if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].versio
         }
 
         input_wrapping do
+          if scope_id
+            rich_file = Rich::RichFile.find(scope_id)
+            img_path = rich_file.rich_file(editor_options[:preview_size])
+          else
+            img_path = editor_options[:placeholder_image]
+          end
+
+          if editor_options[:preview_size]
+            img_width = rich_file.set_styles[editor_options[:preview_size]].split('x').first
+          end
 
           label_html <<
           if editor_options[:hidden_input] == true
             field = builder.hidden_field(method, local_input_options.merge(input_html_options)) 
-            p scope_id.class
-            if scope_id
-              rich_file = Rich::RichFile.find(scope_id)
-              img_path = rich_file.rich_file(editor_options[:preview_size])
-              if editor_options[:preview_size]
-                img_width = rich_file.set_styles[editor_options[:preview_size]].split('x').first
-              end
-            end
           else
             field = builder.text_field(method, local_input_options.merge(input_html_options)) 
-            img_path = editor_options[:placeholder_image]
           end
 
           field  <<
