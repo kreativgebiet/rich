a534d29daf628fc82a1b393ec2b8f37ee53f4e8c
Daniel Westendorf <daniel@prowestech.com>
2012-10-03 11:31:11 -0600

    method called on nil, fixed

--- app/inputs/rich_picker_input.rb
+++ app/inputs/rich_picker_input.rb
@@ -16,12 +16,11 @@ if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].versio
           if scope_id
             rich_file = Rich::RichFile.find(scope_id)
             img_path = rich_file.rich_file(editor_options[:preview_size])
+            if editor_options[:preview_size]
+              img_width = rich_file.set_styles[editor_options[:preview_size]].split('x').first
+            end
           else
             img_path = editor_options[:placeholder_image]
-          end
-
-          if editor_options[:preview_size]
-            img_width = rich_file.set_styles[editor_options[:preview_size]].split('x').first
           end
 
           label_html <<
