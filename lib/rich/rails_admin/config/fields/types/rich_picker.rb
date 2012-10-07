28d4217ed0e2cdf140b330fffbc0d8af42810615
Bastiaan Terhorst <bastiaan@perceptor.nl>
2012-10-07 15:11:57 +0200

    preview size, hidden input and object link support for Rails Admin

--- lib/rich/rails_admin/config/fields/types/rich_picker.rb
+++ lib/rich/rails_admin/config/fields/types/rich_picker.rb
@@ -19,6 +19,33 @@ module RailsAdmin::Config::Fields::Types
     def scope_id
       bindings[:object].id
     end
-    
+
+    def editor_options
+      Rich.options(config, scope_type, scope_id)
+    end
+
+    def image_path
+      Rails.logger.info value
+      Rails.logger.info scope_id
+      Rails.logger.info scope_type
+
+      if value.to_s.html_safe != ""
+        if (true if Float(value) rescue false)
+          Rails.logger.info "numeric"
+          # if the value is numeric we assume its an object id
+          rich_file = Rich::RichFile.find(value)
+          rich_file.rich_file
+        else
+          Rails.logger.info "string"
+          # if not, we assume its a url
+          value.to_s
+        end
+      else
+        # no value, show placeholder image
+        editor_options[:placeholder_image]
+      end
+
+    end
+
   end
 end
