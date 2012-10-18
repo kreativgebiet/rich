726af339814a35152197d4bc6bcbdd23e0bae122
rheaton <rachelmheaton@gmail.com>
2012-10-18 13:49:48 -0400

    formtastic rich picker uses rich files when the rich_picker field is an integer
    Issues:
    - does the javascript understand this and place the ID in there?
    - why is the label not showing up?!
    - need the asset path for document-thumb or css

--- app/inputs/rich_picker_input.rb
+++ app/inputs/rich_picker_input.rb
@@ -1,40 +1,84 @@
 if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].version.version[0,1] == "2")
-    
-    class RichPickerInput < ::Formtastic::Inputs::StringInput
 
-      def to_html 
-        scope_type = object_name
-        scope_id = object.id
-        editor_options = Rich.options(options[:config], scope_type, scope_id)
-        
+    class RichPickerInput < ::Formtastic::Inputs::StringInput
+      include Formtastic::Helpers::InputHelper
+      attr_reader :editor_options
+
+      def to_html
+        @editor_options = Rich.options(options[:config], object_name, object.id)
+
         local_input_options = {
           :class => 'rich-picker',
           :style => editor_options[:style]
         }
 
         input_wrapping do
-          if scope_id
-            rich_file = Rich::RichFile.find(scope_id)
-            img_path = rich_file.rich_file
-          else
-            img_path = editor_options[:placeholder_image]
-          end
-
+          # where is the label!? -RH
           label_html <<
           if editor_options[:hidden_input] == true
-            field = builder.hidden_field(method, local_input_options.merge(input_html_options)) 
+            field = builder.hidden_field(method, local_input_options.merge(input_html_options))
           else
-            field = builder.text_field(method, local_input_options.merge(input_html_options)) 
+            field = builder.text_field(method, local_input_options.merge(input_html_options))
           end
 
-          field  <<
-          " <a href='#{Rich.editor[:richBrowserUrl]}' class='button'>#{I18n.t('picker_browse')}</a>".html_safe <<
-          "</br></br><img class='rich-image-preview' src='#{img_path}' style='max-width:#{editor_options[:preview_size]}; max-height:#{editor_options[:preview_size]}; '/>".html_safe <<
-          "<script>$(function(){$('##{input_html_options[:id]}_input a').click(function(e){ e.preventDefault(); assetPicker.showFinder('##{input_html_options[:id]}', #{editor_options.to_json.html_safe})})})</script>".html_safe
+          field << button
+          field << preview
+          field << javascript
+        end
+      end
+private
+      def image_path
+        method_value = object.send(method)
+        return unless method_value.present?
+        # TODO: need the asset path for this thing--or just use CSS & add a class -RH
+        # return editor_options[:document_thumb] if editor_options[:type].to_s == 'file'
+        return if editor_options[:type].to_s == 'file'
 
+        # using Formtastic::Helpers::InputHelper#column_type
+        #   should be :string or :integer -RH
+        column_type = column_for(method).type
+        if column_type == :integer
+          file = Rich::RichFile.find(method_value)
+          file.rich_file
+        else # should be :string
+          method_value
         end
       end
 
+      def button
+        %Q{
+            <a href='#{Rich.editor[:richBrowserUrl]}' class='button'>
+              #{I18n.t('picker_browse')}
+            </a>
+        }.html_safe
+      end
+
+      def javascript
+        %Q{
+            <script>
+              $(function(){
+                $('##{input_html_options[:id]}_input a').click(function(e){
+                  e.preventDefault(); assetPicker.showFinder('##{input_html_options[:id]}', #{editor_options.to_json})
+                });
+              });
+            </script>
+        }.html_safe
+      end
+
+      def preview
+        path = image_path
+        klass = "class='rich-image-preview'"
+        style = "style='max-width:#{editor_options[:preview_size]}; max-height:#{editor_options[:preview_size]};'"
+        if path
+          %Q{
+             </br></br><img src='#{image_path}' #{klass} #{style} />
+          }.html_safe
+        else
+          %Q{
+             </br></br><div #{klass} #{style}></div>
+          }.html_safe
+        end
+      end
     end
-    
+
 end
