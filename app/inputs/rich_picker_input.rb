a7a795660b1d605a77cba81a756439c5a2f507ec
Jérémy FRERE <frere.jeremy@gmail.com>
2014-01-14 17:38:20 +0100

    Adds RichPickerInput with added :button_class option

--- app/inputs/rich_picker_input.rb
+++ app/inputs/rich_picker_input.rb
@@ -21,7 +21,8 @@ if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].versio
 
         end
       end
-private
+
+    private
 
       def input_field(local_input_options)
         if editor_options[:hidden_input] == true
@@ -85,4 +86,89 @@ private
       end
     end
 
+end
+
+if Object.const_defined?("SimpleForm")
+  class RichPickerInput < SimpleForm::Inputs::StringInput
+    attr_reader :editor_options, :dom_id
+
+    def input
+      @editor_options = Rich.options(options[:config], object_name, object.id)
+      @dom_id = "#{object_name}_#{attribute_name}"
+
+      local_input_options = {
+        :class => 'rich-picker',
+        :style => editor_options[:style]
+      }
+
+      input_field(local_input_options) <<
+      button <<
+      preview <<
+      javascript
+    end
+
+  private
+
+    def input_field(local_input_options)
+      if editor_options[:hidden_input] == true
+        @builder.hidden_field(attribute_name, local_input_options.merge(input_html_options))
+      else
+        @builder.text_field(attribute_name, local_input_options.merge(input_html_options))
+      end
+    end
+
+    def preview_image_path
+      method_value = object.send(attribute_name)
+
+      # return placeholder image if this is a non-image picker OR if there is no value set
+      return editor_options[:placeholder_image] if editor_options[:type].to_s == 'file'
+      return editor_options[:placeholder_image] unless method_value.present?
+
+      column_type = column_for(attribute_name).type
+      if column_type == :integer
+        file = Rich::RichFile.find(method_value)
+        file.rich_file.url(:rich_thumb) #we ask paperclip directly for the file, so asset paths should not be an issue
+      else # should be :string
+        method_value
+      end
+    end
+
+    def button
+      css_class = editor_options[:button_class] || 'button'
+      %Q{
+          <a href='#{Rich.editor[:richBrowserUrl]}' class='#{css_class}'>
+            #{I18n.t('picker_browse')}
+          </a>
+      }.html_safe
+    end
+
+    def javascript
+      %Q{
+          <script>
+            $(function(){
+              $('.#{dom_id} a').click(function(e){
+                e.preventDefault(); assetPicker.showFinder('##{dom_id}', #{editor_options.to_json})
+              });
+            });
+          </script>
+      }.html_safe
+    end
+
+    def preview
+      return unless editor_options[:type] != 'file'
+
+      path = preview_image_path
+      klass = "class='rich-image-preview'"
+      style = "style='max-width:#{editor_options[:preview_size]}; max-height:#{editor_options[:preview_size]};'"
+      if path
+        %Q{
+           </br></br><img src='#{preview_image_path}' #{klass} #{style} />
+        }.html_safe
+      else
+        %Q{
+           </br></br><div #{klass} #{style}></div>
+        }.html_safe
+      end
+    end
+  end
 end
