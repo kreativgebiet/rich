97e9fdb0457e711c9b483602508447e5b7785a35
Andrea Dal Ponte <info@andreadalponte.com>
2014-06-16 14:48:38 +0200

    bindings in some cases may be nil

--- lib/rich/rails_admin/config/fields/types/rich_editor.rb
+++ lib/rich/rails_admin/config/fields/types/rich_editor.rb
@@ -2,17 +2,17 @@
 module RailsAdmin::Config::Fields::Types
   class RichEditor < RailsAdmin::Config::Fields::Types::Text
     RailsAdmin::Config::Fields::Types::register(:rich_editor, self)
-        
+
     register_instance_option(:config) do
       {}
     end
-    
+
     register_instance_option(:partial) do
       :form_rich_text
     end
 
     def scope_type
-
+      return nil unless bindings && bindings[:form]
       # Use association name if existent
       if bindings[:form].object_name.scan(/.*\[(.*)_attributes\]/).nil?
         bindings[:form].object_name
@@ -30,6 +30,5 @@ module RailsAdmin::Config::Fields::Types
     def scope_id
       bindings[:object].id
     end
-
   end
 end
