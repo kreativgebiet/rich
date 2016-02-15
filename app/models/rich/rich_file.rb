f1e2a610fdb811d8c8cbd23dd0b5e80c3314a220
Lozer Ilya <ilozer@ergoserv.com>
2016-02-15 12:37:06 +0200

    Fixed undefined method 'rich_file='

--- app/models/rich/rich_file.rb
+++ app/models/rich/rich_file.rb
@@ -4,6 +4,8 @@ require 'kaminari'
 
 module Rich
   class RichFile < ActiveRecord::Base
+    include Backends::Paperclip
+
     scope :images,  -> { where("rich_rich_files.simplified_type = 'image'") }
     scope :files,   -> { where("rich_rich_files.simplified_type = 'file'") }
 
