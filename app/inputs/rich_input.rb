33eb6d8bcf43a10f07dbefe82c89517ae43fecf1
Ishmael Kay <ishmael.kay@gmail.com>
2017-01-09 10:54:39 +1100

    only using simple form for RichInput

--- app/inputs/rich_input.rb
+++ app/inputs/rich_input.rb
@@ -1,35 +1,10 @@
-if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].version.version[0,1].to_i > 1)
+class RichInput < SimpleForm::Inputs::TextInput
+  def input
+    scope_type = object_name
+    scope_id = object.id
+    editor_options = Rich.options(options[:config], scope_type, scope_id)
 
-  class RichInput < ::Formtastic::Inputs::TextInput
-    def to_html
-
-      scope_type = object_name
-      scope_id = object.id
-      editor_options = Rich.options(
-                                  options[:config],
-                                    options[:config].fetch(:scope_type, scope_type),
-                                    options[:config].fetch(:scope_id, scope_id)
-                                  )
-
-      input_wrapping do
-        label_html <<
-        builder.text_area(method, input_html_options) <<
-        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe
-      end
-    end
+    super <<
+    "<script>CKEDITOR.replace('#{object_name}_#{attribute_name}', #{editor_options.to_json.html_safe});</script>".html_safe
   end
-
 end
-
-if Object.const_defined?("SimpleForm")
-  class RichInput < SimpleForm::Inputs::TextInput
-    def input
-      scope_type = object_name
-      scope_id = object.id
-      editor_options = Rich.options(options[:config], scope_type, scope_id)
-
-      super <<
-      "<script>CKEDITOR.replace('#{object_name}_#{attribute_name}', #{editor_options.to_json.html_safe});</script>".html_safe
-    end
-  end
- end
