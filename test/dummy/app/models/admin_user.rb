5cf4df269539b654f71b535e194b5089fb906c05
Ishmael Kay <ishmael.kay@gmail.com>
2017-01-09 10:40:19 +1100

    fix undefined method attr_accessible

--- test/dummy/app/models/admin_user.rb
+++ test/dummy/app/models/admin_user.rb
@@ -5,5 +5,5 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessor :email, :password, :password_confirmation, :remember_me
 end
