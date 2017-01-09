5e524c2d20f08776c878bf21027bb866f0c56c5e
Ishmael Kay <ishmael.kay@gmail.com>
2017-01-09 16:15:51 +1100

    deprecation warning

--- app/controllers/rich/files_controller.rb
+++ app/controllers/rich/files_controller.rb
@@ -1,8 +1,8 @@
 module Rich
   class FilesController < ApplicationController
 
-    before_filter :authenticate_rich_user
-    before_filter :set_rich_file, only: [:show, :update, :destroy]
+    before_action :authenticate_rich_user
+    before_action :set_rich_file, only: [:show, :update, :destroy]
 
     layout "rich/application"
 
